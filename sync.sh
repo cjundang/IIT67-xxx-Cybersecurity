c6eaa598b62ac0ea5d1532797fb68abb1353c6fd
chanankorn <cjundang@gmail.com>
2025-11-13 08:42:00 +0700

    Initial upload or update of all project files

--- sync.sh
+++ sync.sh
@@ -4,6 +4,8 @@ git status
 # 3. Stage ALL files (tracked and untracked)
 git add .
 
+git config --global user.name "chanankorn"
+git config --global user.email "cjundang@gmail.com"
 # 4. Commit the staged files with a message
 git commit -m "Initial upload or update of all project files"
 
